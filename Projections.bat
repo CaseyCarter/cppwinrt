a11cd27075af466f8cd2f2d25bcce92f90030949
Kevin Welton <kevin.welton@microsoft.com>
2016-03-22 11:00:49 -0700

    Update back-end to use WinMDc2 as exe name, in line with the front-end exe name.

--- Projections.bat
+++ Projections.bat
@@ -1,8 +1,8 @@
 @echo off
 
 FrontEnd\FrontEnd\bin\Release\WinMDc1.exe @Projections.answers
-BackEnd\x86\Release\Modern.exe Complete.db -out Projections\Complete
+BackEnd\x86\Release\WinMDc2.exe Complete.db -out Projections\Complete
 FrontEnd\FrontEnd\bin\Release\WinMDc1.exe @Projections.answers
-BackEnd\x86\Release\Modern.exe Complete.db -out Projections\Minimal
+BackEnd\x86\Release\WinMDc2.exe Complete.db -out Projections\Minimal
 FrontEnd\FrontEnd\bin\Release\WinMDc1.exe @Projections.answers
-BackEnd\x86\Release\Modern.exe Complete.db -out Projections\NoXaml
+BackEnd\x86\Release\WinMDc2.exe Complete.db -out Projections\NoXaml
