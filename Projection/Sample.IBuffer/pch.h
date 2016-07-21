3d9fc278ffd8add64df4705ee736c6282f5d985c
Kevin Welton <kevin.welton@microsoft.com>
2016-07-21 16:11:03 -0700

    Add sample project showing use of IBufferByteAccess in C++/WinRT

---
+++ Projection/Sample.IBuffer/pch.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <robuffer.h>
+
+#include <winrt\Windows.Storage.Pickers.h>
+#include <winrt\Windows.Storage.Streams.h>
+#include <winrt\Windows.UI.Core.h>
+#include <winrt\Windows.UI.Xaml.Controls.h>
+#include <winrt\Windows.UI.Xaml.Media.Imaging.h>
+#include <winrt\Windows.Graphics.Imaging.h>
+
+#include <future>
