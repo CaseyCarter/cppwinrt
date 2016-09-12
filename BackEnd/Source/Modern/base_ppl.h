0af3fccfdcdb57c5eb77a88dd89de4fa9e1d32a0
Kevin Welton <kevin.welton@microsoft.com>
2016-09-12 09:59:30 -0700

    Update PPL to use correct context for result objects

--- BackEnd/Source/Modern/base_ppl.h
+++ BackEnd/Source/Modern/base_ppl.h
@@ -1,28 +1,79 @@
 
 #include "base.h"
+#include <ctxtcall.h>
 #include <ppltasks.h>
 
 namespace winrt::ppl
 {
+    struct adapter
+    {
+        adapter() noexcept = default;
+
+        adapter(Windows::IUnknown const & object) : m_object(object)
+        {
+            m_object = object;
+            m_agile = !!object.try_as<IAgileObject>();
+
+            if (!m_agile)
+            {
+                check_hresult(::CoGetObjectContext(__uuidof(m_context), reinterpret_cast<void **>(put(m_context))));
+            }
+        };
+
+        template<typename T>
+        operator T() const
+        {
+            if (m_agile)
+            {
+                return m_object.as<T>();
+            }
+
+            struct user_data
+            {
+                IUnknown * object;
+                IStream * stream;
+            };
+
+            user_data data{ winrt::get(m_object) };
+            ComCallData param{ 0, 0, &data };
+
+            check_hresult(m_context->ContextCallback([](ComCallData * param)
+            {
+                user_data & data = *static_cast<user_data *>(param->pUserDefined);
+
+                return CoMarshalInterThreadInterfaceInStream(__uuidof(abi_default_interface<T>),
+                    data.object,
+                    &data.stream);
+            }, &param, __uuidof(IContextCallback), 5, nullptr));
+
+            T result = nullptr;
+
+            check_hresult(CoGetInterfaceAndReleaseStream(data.stream,
+                __uuidof(abi_default_interface<T>),
+                reinterpret_cast<void **>(put(result))));
+
+            return result;
+        }
+
+    private:
+        Windows::IUnknown m_object;
+        bool m_agile = false;
+        com_ptr<IContextCallback> m_context;
+    };
+
     inline auto create_task(Windows::Foundation::IAsyncAction const & async)
     {
         concurrency::task_completion_event<void> event;
 
-        async.Completed([event](Windows::Foundation::IAsyncAction const & sender,
-                                Windows::Foundation::AsyncStatus)
+        async.Completed([event](auto && ...)
         {
-            try
-            {
-                sender.GetResults();
-                event.set();
-            }
-            catch (...)
-            {
-                event.set_exception(std::current_exception());
-            }
+            event.set();
         });
 
-        return concurrency::task<void>(event);
+        return concurrency::task<void>(event).then([=]
+        {
+            async.GetResults();
+        });
     }
 
     template <typename TProgress>
@@ -30,62 +81,49 @@ namespace winrt::ppl
     {
         concurrency::task_completion_event<void> event;
 
-        async.Completed([event](Windows::Foundation::IAsyncActionWithProgress<TProgress> const & sender,
-                                Windows::Foundation::AsyncStatus)
+        async.Completed([event](auto && ...)
         {
-            try
-            {
-                sender.GetResults();
-                event.set();
-            }
-            catch (...)
-            {
-                event.set_exception(std::current_exception());
-            }
+            event.set();
         });
 
-        return concurrency::task<void>(event);
+        return concurrency::task<void>(event).then([=]
+        {
+            async.GetResults();
+        });
     }
 
     template <typename TResult>
     auto create_task(Windows::Foundation::IAsyncOperation<TResult> const & async)
     {
-        concurrency::task_completion_event<TResult> event;
+        using adapted_result = std::conditional_t<impl::is_base_of_v<Windows::IUnknown, TResult>, adapter, TResult>;
 
-        async.Completed([event](Windows::Foundation::IAsyncOperation<TResult> const & sender,
-                                Windows::Foundation::AsyncStatus)
+        concurrency::task_completion_event<void> event;
+
+        async.Completed([event](auto && ...)
         {
-            try
-            {
-                event.set(sender.GetResults());
-            }
-            catch (...)
-            {
-                event.set_exception(std::current_exception());
-            }
+            event.set();
         });
 
-        return concurrency::task<TResult>(event);
+        return concurrency::task<void>(event).then([=]
+        {
+            return concurrency::create_task([=]() -> adapted_result { return async.GetResults(); });
+        });
     }
 
     template <typename TResult, typename TProgress>
-    auto create_task(Windows::Foundation::IAsyncOperationWithProgress<TResult, TProgress> const & operation)
+    auto create_task(Windows::Foundation::IAsyncOperationWithProgress<TResult, TProgress> const & async)
     {
-        concurrency::task_completion_event<TResult> event;
+        using adapted_result = std::conditional_t<impl::is_base_of_v<Windows::IUnknown, TResult>, adapter, TResult>;
+        concurrency::task_completion_event<void> event;
 
-        operation.Completed([event](Windows::Foundation::IAsyncOperationWithProgress<TResult, TProgress> const & sender,
-                                    Windows::Foundation::AsyncStatus)
+        async.Completed([event](auto && ...)
         {
-            try
-            {
-                event.set(sender.GetResults());
-            }
-            catch (...)
-            {
-                event.set_exception(std::current_exception());
-            }
+            event.set();
         });
 
-        return concurrency::task<TResult>(event);
+        return concurrency::task<void>(event).then([=]
+        {
+            return concurrency::create_task([=]() -> adapted_result { return async.GetResults(); });
+        });
     }
 }
