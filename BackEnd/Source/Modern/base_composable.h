a6cb7477d3d13ba91599fa8d5fbd3c66a9da6132
Kenny Kerr <kekerr@microsoft.com>
2017-02-02 15:50:21 -0800

    Clang picked up two build breaks with the base library.

--- BackEnd/Source/Modern/base_composable.h
+++ BackEnd/Source/Modern/base_composable.h
@@ -14,7 +14,7 @@ struct overrides : implements<D, R ...>
 
         if (result == E_NOINTERFACE)
         {
-            result = m_inner->QueryInterface(id, object);
+            result = get(m_inner)->QueryInterface(id, object);
         }
 
         return result;
--- BackEnd/Source/Modern/base_meta.h
+++ BackEnd/Source/Modern/base_meta.h
@@ -31,16 +31,16 @@ struct conjunction<B1, Bn ...> : std::conditional_t<B1::value, conjunction<Bn ..
 template<typename ... B>
 constexpr bool conjunction_v = conjunction<B ...>::value;
 
-template<typename T, T First, T... Rest>
-constexpr bool sequence_contains(T value)
-{
-    return (value == First) || sequence_contains<T, Rest...>(value);
-}
-
 template<typename T>
 constexpr bool sequence_contains(T)
 {
     return false;
+}
+
+template<typename T, T First, T... Rest>
+constexpr bool sequence_contains(T value)
+{
+    return (value == First) || sequence_contains<T, Rest...>(value);
 }
 
 }
