9fbdb8f71a851ce8ed0da7b56d82603969ee5517
Ryan Shepherd <ryansh@microsoft.com>
2017-02-22 15:03:41 -0800

    Improve reliability and debuggability of clock test

--- Projection/Tests/clock.cpp
+++ Projection/Tests/clock.cpp
@@ -17,13 +17,40 @@ namespace
     }
 }
 
+namespace Catch
+{
+    template<typename Rep, typename Period>
+    std::string toString(const duration<Rep, Period>& value_)
+    {
+        nanoseconds value = value_;
+        std::string result;
+        if (value >= seconds{ 1 })
+        {
+            auto count = duration_cast<seconds>(value).count();
+            result.append(std::to_string(count));
+            result.append(" seconds");
+            value %= seconds{ 1 };
+        }
+        assert(value < seconds{ 1 });
+        if (value >= nanoseconds{ 1 })
+        {
+            auto count = duration_cast<nanoseconds>(value).count();
+            if (!result.empty()) { result.append(", "); }
+            result.append(std::to_string(count));
+            result.append(" nanoseconds");
+            value %= nanoseconds{ 1 };
+        }
+        return result;
+    }
+}
+
 TEST_CASE("clock, now")
 {
     Calendar calendar;
     calendar.SetToNow();
     auto time = clock::now();
     auto diff = calendar.GetDateTime() - time;
-    REQUIRE(abs(diff) < milliseconds{ 1 });
+    REQUIRE(abs(diff) < milliseconds{ 100 });
 }
 
 TEST_CASE("clock, units")
@@ -66,5 +93,5 @@ TEST_CASE("clock, FILETIME")
     // Verify epoch
     ::GetSystemTimePreciseAsFileTime(&now_ft);
     const auto diff = abs(clock::now() - clock::from_FILETIME(now_ft));
-    REQUIRE(diff < milliseconds{ 1 });
+    REQUIRE(diff < milliseconds{ 100 });
 }
