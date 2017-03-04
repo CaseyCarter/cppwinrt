acf2d6a79e227b84c851af79c3e1bd37883cb755
Scott Jones <sjones@microsoft.com>
2017-03-03 16:07:28 -0800

    Specify directory on copying zip files

--- Publish.cmd
+++ Publish.cmd
@@ -24,14 +24,14 @@ for %%x in (exe pdb) do XCOPY BackEnd\%BuildPlatform%\%BuildConfiguration%\*.%%x
 for %%x in (dll exe pdb) do XCOPY FrontEnd\FrontEnd\bin\%BuildConfiguration%\*.%%x compiler /D /R /Y /J /I
 XCOPY FrontEnd\FrontEnd\bin\%BuildConfiguration%\x86\*.dll compiler\x86 /D /R /Y /J /I
 powershell -ExecutionPolicy ByPass -Command "& '%cd%\zip.ps1' -directory '%cd%\compiler' -name 'compiler.zip'"
-XCOPY compiler.zip %PublishShare% /D /R /Y /J 
+echo d | XCOPY compiler.zip %PublishShare% /D /R /Y /J 
 
 echo Publish GitHub Format 
 XCOPY generated\winrt stage\winrt /D /S /R /Y /J /I 
 set PUBLIC_SAMPLES=AsyncReader Blocks CL IBuffer JustCoroutines Ocr Syndication Video XamlCode
 for %%x in (%PUBLIC_SAMPLES%) do XCOPY projection\Samples\%%x stage\Samples\%%x /D /S /R /Y /J /I 
 powershell -ExecutionPolicy ByPass -Command "& '%cd%\zip.ps1' -directory '%cd%\stage' -name 'github.zip'"
-XCOPY github.zip %PublishShare% /D /R /Y /J 
+echo d | XCOPY github.zip %PublishShare% /D /R /Y /J 
 
 echo Publish Projection 
 XCOPY projection\Samples stage\Samples /D /S /R /Y /J /I 
@@ -39,4 +39,4 @@ XCOPY projection\SDKReferences stage\SDKReferences /D /S /R /Y /J /I
 XCOPY projection\SDKResponseFiles stage\SDKResponseFiles /D /S /R /Y /J /I 
 XCOPY projection\Tests stage\Tests /D /R /Y /J /I 
 powershell -ExecutionPolicy ByPass -Command "& '%cd%\zip.ps1' -directory '%cd%\stage' -name 'projection.zip'"
-XCOPY projection.zip %PublishShare% /D /R /Y /J 
+echo d | XCOPY projection.zip %PublishShare% /D /R /Y /J 
