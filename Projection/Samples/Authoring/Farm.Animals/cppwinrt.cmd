de09d7b74427af15b431f75b329f73b05512dd42
Scott Jones <sjones@microsoft.com>
2017-05-16 15:13:29 +0000

    Merged PR 463675: Fix last few VSO build issues (debug, samples)
    Fix last few VSO build issues (debug, samples)

--- Projection/Samples/Authoring/Farm.Animals/cppwinrt.cmd
+++ Projection/Samples/Authoring/Farm.Animals/cppwinrt.cmd
@@ -1,4 +1,9 @@
 @echo off
 echo ------ Running C++/WinRT compiler ------
-rem ..\..\..\..\cppwinrt\x86\release\cppwinrt.exe %* -w
-..\..\..\..\cppwinrt\x86\release\cppwinrt.exe %* -v
+setlocal
+set compiler=..\..\..\..\cppwinrt\x86\release\cppwinrt.exe
+if not exist %compiler% (
+set compiler=..\..\..\..\cppwinrt\x86\debug\cppwinrt.exe
+)
+rem %compiler% %* -w
+%compiler% %* -v
