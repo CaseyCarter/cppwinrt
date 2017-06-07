80b4dccf39bf5b78f38c55ea3d6b6b3bae653e6f
Scott Jones <sjones@microsoft.com>
2017-06-07 19:29:05 +0000

    Merged PR 531816: Disable optimizations for 2017.3 Preview to work around generator bug
    Disable optimizations for 2017.3 Preview to work around generator bug
    
    Related work items: #12254432

--- BuildCompiler.cmd
+++ BuildCompiler.cmd
@@ -3,8 +3,14 @@
 call SetBuildVars.cmd %*
 
 pushd cppwinrt
+
+rem Clean targets
+for /f %%i in ('dir /s /b %BuildConfiguration%') do @rd %%i /s /q >nul
+
 echo Building compiler for %BuildConfiguration% %BuildPlatform%...
 msbuild "cppwinrt.sln" /nologo /m /p:Configuration=%BuildConfiguration% /p:Platform=%BuildPlatform% 
 set MsbuildErrorLevel=%ERRORLEVEL%
+
 popd
+
 exit /B %MsbuildErrorLevel%
--- cppwinrt/source/pch.h
+++ cppwinrt/source/pch.h
@@ -1,14 +1,22 @@
 #pragma once
 
+// VS 2017.3 Preview has a generator bug that causes premature destruction of the coroutine frame 
+// in release (optimized) builds.  Work around by disabling optimizations for this version.
+#if _MSC_FULL_VER == 191125303
+#pragma message("WARNING: Build is unoptimized, to work around generator bug in VS 2017.3 Preview")
+#pragma optimize("", off)
+#endif
+
 #include <cor.h>
 #include <corerror.h>
 #include <corhdr.h>
 
 #include "base.h"
-#include <experimental/generator>
 #include <functional>
 #include <rometadataapi.h>
 #include <rometadata.h>
 #include <filesystem>
 #include <optional>
 #include <set>
+
+#include "generator.h"
