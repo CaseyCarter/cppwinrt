a992d00c9b3be187b7056cb59502059830decf33
Kenny Kerr <kekerr@microsoft.com>
2017-08-22 20:36:53 +0000

    Merged PR 797388: Add Win2D sample
    A sample XAML app that uses Win2D for rendering. It illustrates importing a 3rd party component for use within the app.

---
+++ Projection/Samples/Win2DXaml/cppwinrt.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo ------ Running C++/WinRT compiler ------
+setlocal
+set compiler=..\..\..\cppwinrt\x86\release\cppwinrt.exe
+if not exist %compiler% (
+set compiler=..\..\..\cppwinrt\x86\debug\cppwinrt.exe
+)
+%compiler% -in packages\Win2D.uwp.1.21.0\lib\uap10.0\Microsoft.Graphics.Canvas.winmd -ref 10.0.15063.0 -verbose -out Win2D
+copy App.winmd %*
