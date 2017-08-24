56c8184e85eb95fa6bb1b1a325faadf48ab091d8
Kenny Kerr <kekerr@microsoft.com>
2017-08-24 22:10:15 +0000

    Merged PR 806846: Interop sample
    Interop sample that illustrates how to convert between C++/WinRT, C++/CX and ABI types.

---
+++ Projection/Samples/Interop/Main.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+
+template <typename T>
+T^ to_cx(winrt::Windows::Foundation::IUnknown const& from)
+{
+    return safe_cast<T^>(reinterpret_cast<Platform::Object^>(winrt::get_abi(from)));
+}
+
+template <typename T>
+T from_cx(Platform::Object^ from)
+{
+    T to{ nullptr };
+    winrt::check_hresult(reinterpret_cast<::IUnknown*>(from)->QueryInterface(winrt::put_abi(to)));
+    return to;
+}
+
+template <typename T>
+T from_abi(::IUnknown* from)
+{
+    T to{ nullptr };
+    winrt::check_hresult(from->QueryInterface(winrt::put_abi(to)));
+    return to;
+}
+
+namespace cx
+{
+    using namespace Windows::Foundation;
+}
+
+namespace winrt
+{
+    using namespace Windows::Foundation;
+}
+
+namespace abi
+{
+    using namespace ABI::Windows::Foundation;
+};
+
+int main()
+{
+    winrt::init_apartment();
+
+    winrt::Uri cpp(L"http://moderncpp.com/");
+    printf("C++/WinRT: %ls\n", cpp.Domain().c_str());
+
+    cx::Uri ^ cx = to_cx<cx::Uri>(cpp);
+    printf("C++/CX: %ls\n", cx->Domain->Data());
+
+    winrt::com_ptr<abi::IUriRuntimeClass> abi = cpp.as<abi::IUriRuntimeClass>();
+    winrt::hstring domain;
+    winrt::check_hresult(abi->get_Domain(put_abi(domain)));
+    printf("ABI: %ls\n", domain.c_str());
+
+    winrt::Uri cpp_from_cx = from_cx<winrt::Uri>(cx);
+    WINRT_ASSERT(cpp == cpp_from_cx);
+
+    winrt::Uri cpp_from_abi = from_abi<winrt::Uri>(abi.get());
+    WINRT_ASSERT(cpp == cpp_from_abi);
+}
---
+++ Projection/Samples/Interop/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#pragma comment(lib, "windowsapp") 
+
+#include <windows.foundation.h>
+#include <winrt/Windows.Foundation.h>
