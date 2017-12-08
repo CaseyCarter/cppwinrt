c799083f7fdf16c6e8b5f5bf103bbeb837a331be
Scott Jones <sjones@microsoft.com>
2017-12-08 21:10:06 +0000

    Merged PR 1196729: Clean compile tests when building projection
    Clean compile tests when building projection (note: this can't be done by RunCompileTests.cmd, which simply builds the compile tests)
    
    Related work items: #14858631

--- BuildProjection.cmd
+++ BuildProjection.cmd
@@ -7,4 +7,6 @@ set compiler=%~dp0\cppwinrt\%BuildPlatform%\%BuildConfiguration%\cppwinrt.exe
 set output=%~dp0\%BuildStaging%
 
 rd %output%\winrt /S /Q
+rd %output%\compiletests /S /Q
+
 %compiler% -in 10.0.16299.0 -verbose -tests -brackets -out %output%
