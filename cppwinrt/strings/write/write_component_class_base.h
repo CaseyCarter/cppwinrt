6c04c2dda110c2c8af22a8baaaa33efe7192f841
Ryan Shepherd <ryansh@microsoft.com>
2017-12-16 02:03:13 +0000

    Merged PR 1228386: Build fix for inheriting dependent template constructors.
    Build fix for inheriting dependent constructors. The new conformance mode (/permissive-) does proper two-phase lookup, but I still needed to have code that worked without conformance mode.
    
    Enter this Jedi mind trick to force the compiler to see implements_type as a dependent type, so I can then inherit its constructor.
    
    Related work items: #14155007

--- cppwinrt/strings/write/write_component_class_base.h
+++ cppwinrt/strings/write/write_component_class_base.h
@@ -4,7 +4,8 @@ struct WINRT_EBO %_base : implements<D%@, %I...>%%%
 {
     using base_type = %_base;
     using class_type = @;
-    using %_base::implements_type::implements_type;
+    using implements_type = typename %_base::implements_type;
+    using implements_type::implements_type;
     @
     operator class_type() const noexcept
     {
