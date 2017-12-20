997c5791defe564147992db06e1b19d45c35d8d9
Scott Jones <sjones@microsoft.com>
2017-12-20 21:45:51 +0000

    Merged PR 1238847: Edit missing from last checkin
    Edit missing from last checkin

--- BuildVsix.cmd
+++ BuildVsix.cmd
@@ -12,9 +12,14 @@ for /f %%i in ('dir /s /b %BuildConfiguration%') do @rd %%i /s /q >nul
 echo Updating VSIX manifest version from cppwinrt\source\version.h
 powershell -f UpdateVersion.ps1
 
-echo Building VSIX for %BuildConfiguration% %BuildPlatform%...
-msbuild vsix.csproj /nologo /m /p:Configuration=%BuildConfiguration% /p:Platform=%BuildPlatform% 
+echo Building Component VSIX for %BuildConfiguration% %BuildPlatform%...
+msbuild vsix.csproj /nologo /m /p:Configuration=%BuildConfiguration% /p:Platform=%BuildPlatform% /p:IsProductComponent=true
 set MsbuildErrorLevel=%ERRORLEVEL%
+if %MsbuildErrorLevel%==0 (
+echo Building Standalone VSIX for %BuildConfiguration% %BuildPlatform%...
+msbuild vsix.csproj /nologo /m /p:Configuration=%BuildConfiguration% /p:Platform=%BuildPlatform%  /p:IsProductComponent=false
+set MsbuildErrorLevel=%ERRORLEVEL%
+)
 
 popd
 
