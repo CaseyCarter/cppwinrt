a8c261aafd5e0bae73c6450c2ca4ff157c34a6ac
Scott Jones <sjones@microsoft.com>
2018-01-02 19:43:24 +0000

    Merged PR 1267141: Delete zip files prior to creating
    Delete zip files prior to creating

--- Publish.cmd
+++ Publish.cmd
@@ -9,6 +9,7 @@ rem publish $(BuildPlatform) $(BuildConfiguration) "Projection" \\redmond\osg\Th
 call SetBuildVars.cmd %*
 
 set PublishShare=%4\%BuildPlatform%\%BuildConfiguration%
+md %PublishShare%
 
 echo Publish Component VSIX and ID for VS installer integration
 echo d | XCOPY vsix\bin\%BuildConfiguration%\component\Microsoft.Windows.CppWinRT.vsix %PublishShare%\vsix\component /D /R /Y /J 
@@ -25,10 +26,9 @@ echo Skipping publish for %BuildPlatform% %BuildConfiguration%
 goto :eof
 
 :publish
-md %PublishShare%
-
 echo Publishing Compiler
 for %%x in (exe pdb) do XCOPY CppWinRT\%BuildPlatform%\%BuildConfiguration%\*.%%x compiler /D /R /Y /J /I
+del compiler.zip 
 powershell -ExecutionPolicy ByPass -Command "& '%~dp0\zip.ps1' -directory '%~dp0\compiler' -name 'compiler.zip'"
 echo d | XCOPY compiler.zip %PublishShare% /D /R /Y /J 
 
@@ -36,6 +36,7 @@ echo Publish GitHub distribution
 XCOPY %BuildStaging%\winrt stage\winrt /D /S /R /Y /J /I 
 set PUBLIC_SAMPLES=Blocks CL CustomBuffer Direct2D Interop JustCoroutines Ocr Syndication Video XamlCalendar XamlCode XamlMarkup
 for %%x in (%PUBLIC_SAMPLES%) do XCOPY Projection\Samples\%%x stage\Samples\%%x /D /S /R /Y /J /I 
+del github.zip
 powershell -ExecutionPolicy ByPass -Command "& '%~dp0\zip.ps1' -directory '%~dp0\stage' -name 'github.zip'"
 echo d | XCOPY github.zip %PublishShare% /D /R /Y /J 
 
@@ -43,5 +44,6 @@ echo Publish entire projection (github + remaining samples and unit tests)
 XCOPY compiler stage /D /R /Y /J /I
 XCOPY Projection\Samples stage\Samples /D /S /R /Y /J /I 
 XCOPY Projection\UnitTests stage\Tests /D /R /Y /J /I 
+del projection.zip
 powershell -ExecutionPolicy ByPass -Command "& '%~dp0\zip.ps1' -directory '%~dp0\stage' -name 'projection.zip'"
 echo d | XCOPY projection.zip %PublishShare% /D /R /Y /J 
